370771f876ada830d2066d566d3333ffd09e2e20
Matthieu Dartiailh <m.dartiailh@gmail.com>
2021-06-22 21:14:40 +0200

    tests: add minimal tests for the tokenizer

---
+++ tests/test_tokenizer.py
@@ -0,0 +1,34 @@
+import io
+from tokenize import NAME, NEWLINE, NUMBER, OP, TokenInfo, generate_tokens
+from typing import Any, Dict, Type
+
+from pegen.tokenizer import Tokenizer
+
+
+def test_peek_getnext():
+    source = io.StringIO("# test\n1")
+    t = Tokenizer(generate_tokens(source.readline))
+    assert t.peek() == TokenInfo(NUMBER, "1", (2, 0), (2, 1), "1")
+    assert t.getnext() == TokenInfo(NUMBER, "1", (2, 0), (2, 1), "1")
+    assert t.peek() == TokenInfo(NEWLINE, "", (2, 1), (2, 2), "")
+    assert t.getnext() == TokenInfo(NEWLINE, "", (2, 1), (2, 2), "")
+
+
+def test_mark_reset():
+    source = io.StringIO("\n1 2")
+    t = Tokenizer(generate_tokens(source.readline))
+    index = t.mark()
+    assert t.peek() == TokenInfo(NUMBER, "1", (2, 0), (2, 1), "1 2")
+    assert t.getnext() == TokenInfo(NUMBER, "1", (2, 0), (2, 1), "1 2")
+    t.reset(index)
+    assert t.peek() == TokenInfo(NUMBER, "1", (2, 0), (2, 1), "1 2")
+    assert t.getnext() == TokenInfo(NUMBER, "1", (2, 0), (2, 1), "1 2")
+
+
+def test_last_non_whitespace():
+    source = io.StringIO("\n1\n2")
+    t = Tokenizer(generate_tokens(source.readline))
+    assert t.peek() == TokenInfo(NUMBER, "1", (2, 0), (2, 1), "1\n")
+    assert t.getnext() == TokenInfo(NUMBER, "1", (2, 0), (2, 1), "1\n")
+    assert t.getnext() == TokenInfo(NEWLINE, "\n", (2, 1), (2, 2), "1\n")
+    assert t.get_last_non_whitespace_token() == TokenInfo(NUMBER, "1", (2, 0), (2, 1), "1\n")
