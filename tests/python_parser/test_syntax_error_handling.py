8299260305d047400d1a25b54e8207e5fa6a47de
Matthieu Dartiailh <m.dartiailh@gmail.com>
2022-04-07 21:35:25 +0200

    bpo-46838

--- tests/python_parser/test_syntax_error_handling.py
+++ tests/python_parser/test_syntax_error_handling.py
@@ -54,22 +54,35 @@ def parse_invalid_syntax(
     assert message in str(e.exconly())
 
     # Check start/end line/column on Python 3.10
-    if sys.version_info >= min_python_version:
-        for parser, exc in [("Python", py_exc), ("pegen", e.value)]:
-            if (
-                exc.lineno != start[0]
-                or exc.offset != start[1]
-                # Do not check end for indentation errors
-                or (not isinstance(e, IndentationError) and exc.end_lineno != end[0])
-                or (
-                    not isinstance(e, IndentationError)
-                    and (end[1] is not None and exc.end_offset != end[1])
-                )
-            ):
+
+    for parser, exc in ([("Python", py_exc)] if sys.version_info >= min_python_version else []) + [
+        ("pegen", e.value)
+    ]:
+        if (
+            exc.lineno != start[0]
+            or exc.offset != start[1]
+            # Do not check end for indentation errors
+            or (
+                sys.version_info >= (3, 10)
+                and not isinstance(e, IndentationError)
+                and exc.end_lineno != end[0]
+            )
+            or (
+                sys.version_info >= (3, 10)
+                and not isinstance(e, IndentationError)
+                and (end[1] is not None and exc.end_offset != end[1])
+            )
+        ):
+            if sys.version_info >= (3, 10):
                 raise ValueError(
                     f"Expected locations of {start} and {end}, but got "
                     f"{(exc.lineno, exc.offset)} and {(exc.end_lineno, exc.end_offset)} "
                     f"from {parser}"
+                )
+            else:
+                raise ValueError(
+                    f"Expected locations of {start}, but got "
+                    f"{(exc.lineno, exc.offset)} from {parser}"
                 )
 
 
@@ -397,6 +410,132 @@ def test_invalid_comprehension(
             (3, 11),
         ),
         (
+            "def foo(/,a,b=,c):\n\tpass",
+            "at least one argument must precede /",
+            (1, 9),
+            (1, 10),
+            (3, 11),
+        ),
+        ("def foo(a,/,/,b,c):\n\tpass", "/ may appear only once", (1, 13), (1, 14), (3, 11)),
+        ("def foo(a,/,a1,/,b,c):\n\tpass", "/ may appear only once", (1, 16), (1, 17), (3, 11)),
+        ("def foo(a=1,/,/,*b,/,c):\n\tpass", "/ may appear only once", (1, 15), (1, 16), (3, 11)),
+        ("def foo(a,/,a1=1,/,b,c):\n\tpass", "/ may appear only once", (1, 18), (1, 19), (3, 11)),
+        ("def foo(a,*b,c,/,d,e):\n\tpass", "/ must be ahead of *", (1, 16), (1, 17), (3, 11)),
+        ("def foo(a=1,*b,c=3,/,d,e):\n\tpass", "/ must be ahead of *", (1, 20), (1, 21), (3, 11)),
+        (
+            "def foo(a,*b=3,c):\n\tpass",
+            "var-positional argument cannot have default value",
+            (1, 13),
+            (1, 14),
+            (3, 11),
+        ),
+        (
+            "def foo(a,*b: int=,c):\n\tpass",
+            "var-positional argument cannot have default value",
+            (1, 18),
+            (1, 19),
+            (3, 11),
+        ),
+        (
+            "def foo(a,**b=3):\n\tpass",
+            "var-keyword argument cannot have default value",
+            (1, 14),
+            (1, 15),
+            (3, 11),
+        ),
+        (
+            "def foo(a,**b: int=3):\n\tpass",
+            "var-keyword argument cannot have default value",
+            (1, 19),
+            (1, 20),
+            (3, 11),
+        ),
+        (
+            "def foo(a,*a, b, **c, d):\n\tpass",
+            "arguments cannot follow var-keyword argument",
+            (1, 23),
+            (1, 24),
+            (3, 11),
+        ),
+        (
+            "def foo(a,*a, b, **c, d=4):\n\tpass",
+            "arguments cannot follow var-keyword argument",
+            (1, 23),
+            (1, 24),
+            (3, 11),
+        ),
+        (
+            "def foo(a,*a, b, **c, *d):\n\tpass",
+            "arguments cannot follow var-keyword argument",
+            (1, 23),
+            (1, 24),
+            (3, 11),
+        ),
+        (
+            "def foo(a,*a, b, **c, **d):\n\tpass",
+            "arguments cannot follow var-keyword argument",
+            (1, 23),
+            (1, 25),
+            (3, 11),
+        ),
+        (
+            "def foo(a=1,/,**b,/,c):\n\tpass",
+            "arguments cannot follow var-keyword argument",
+            (1, 19),
+            (1, 20),
+            (3, 11),
+        ),
+        ("def foo(*b,*d):\n\tpass", "* argument may appear only once", (1, 12), (1, 13), (3, 11)),
+        (
+            "def foo(a,*b,c,*d,*e,c):\n\tpass",
+            "* argument may appear only once",
+            (1, 16),
+            (1, 17),
+            (3, 11),
+        ),
+        (
+            "def foo(a,b,/,c,*b,c,*d,*e,c):\n\tpass",
+            "* argument may appear only once",
+            (1, 22),
+            (1, 23),
+            (3, 11),
+        ),
+        (
+            "def foo(a,b,/,c,*b,c,*d,**e):\n\tpass",
+            "* argument may appear only once",
+            (1, 22),
+            (1, 23),
+            (3, 11),
+        ),
+        (
+            "def foo(a=1,/*,b,c):\n\tpass",
+            "expected comma between / and *",
+            (1, 14),
+            (1, 15),
+            (3, 11),
+        ),
+        (
+            "def foo(a=1,d=,c):\n\tpasss",
+            "expected default value expression",
+            (1, 14),
+            (1, 15),
+            (3, 11),
+        ),
+        (
+            "def foo(a,d=,c):\n\tpass",
+            "expected default value expression",
+            (1, 12),
+            (1, 13),
+            (3, 11),
+        ),
+        (
+            "def foo(a,d: int=,c):\n\tpass",
+            "expected default value expression",
+            (1, 17),
+            (1, 18),
+            (3, 11),
+        ),
+        (
             "lambda x=1, y: x",
             "non-default argument follows default argument",
             (1, 13),
@@ -438,6 +577,87 @@ def test_invalid_comprehension(
             (1, 17),
             (3, 11),
         ),
+        ("lambda /,a,b,c: None", "at least one argument must precede /", (1, 8), (1, 9), (3, 11)),
+        ("lambda a,/,/,b,c: None", "/ may appear only once", (1, 12), (1, 13), (3, 11)),
+        ("lambda a,/,a1,/,b,c: None", "/ may appear only once", (1, 15), (1, 16), (3, 11)),
+        ("lambda a=1,/,/,*b,/,c: None", "/ may appear only once", (1, 14), (1, 15), (3, 11)),
+        ("lambda a,/,a1=1,/,b,c: None", "/ may appear only once", (1, 17), (1, 18), (3, 11)),
+        ("lambda a,*b,c,/,d,e: None", "/ must be ahead of *", (1, 15), (1, 16), (3, 11)),
+        ("lambda a=1,*b,c=3,/,d,e: None", "/ must be ahead of *", (1, 19), (1, 20), (3, 11)),
+        ("lambda a=1,/*,b,c: None", "expected comma between / and *", (1, 13), (1, 14), (3, 11)),
+        (
+            "lambda a,*b=3,c: None",
+            "var-positional argument cannot have default value",
+            (1, 12),
+            (1, 13),
+            (3, 11),
+        ),
+        (
+            "lambda a,**b=3: None",
+            "var-keyword argument cannot have default value",
+            (1, 13),
+            (1, 14),
+            (3, 11),
+        ),
+        (
+            "lambda a, *a, b, **c, d: None",
+            "arguments cannot follow var-keyword argument",
+            (1, 23),
+            (1, 24),
+            (3, 11),
+        ),
+        (
+            "lambda a,*a, b, **c, d=4: None",
+            "arguments cannot follow var-keyword argument",
+            (1, 22),
+            (1, 23),
+            (3, 11),
+        ),
+        (
+            "lambda a,*a, b, **c, *d: None",
+            "arguments cannot follow var-keyword argument",
+            (1, 22),
+            (1, 23),
+            (3, 11),
+        ),
+        (
+            "lambda a,*a, b, **c, **d: None",
+            "arguments cannot follow var-keyword argument",
+            (1, 22),
+            (1, 24),
+            (3, 11),
+        ),
+        (
+            "lambda a=1,/,**b,/,c: None",
+            "arguments cannot follow var-keyword argument",
+            (1, 18),
+            (1, 19),
+            (3, 11),
+        ),
+        ("lambda *b,*d: None", "* argument may appear only once", (1, 11), (1, 12), (3, 11)),
+        (
+            "lambda a,*b,c,*d,*e,c: None",
+            "* argument may appear only once",
+            (1, 15),
+            (1, 16),
+            (3, 11),
+        ),
+        (
+            "lambda a,b,/,c,*b,c,*d,*e,c: None",
+            "* argument may appear only once",
+            (1, 21),
+            (1, 22),
+            (3, 11),
+        ),
+        (
+            "lambda a,b,/,c,*b,c,*d,**e: None",
+            "* argument may appear only once",
+            (1, 21),
+            (1, 22),
+            (3, 11),
+        ),
+        ("lambda a=1,d=,c: None", "expected default value expression", (1, 13), (1, 14), (3, 11)),
+        ("lambda a,d=,c: None", "expected default value expression", (1, 11), (1, 12), (3, 11)),
     ],
 )
 def test_invalid_parameters(
