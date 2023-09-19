476e8f73e4e84853d3c0fb91ae0eaeb020318dde
MatthieuDartiailh <m.dartiailh@gmail.com>
2023-09-19 16:54:48 +0200

    python.gram: get test_ast_parsing to pass on 3.12

--- tests/python_parser/data/assignment.py
+++ tests/python_parser/data/assignment.py
@@ -14,12 +14,16 @@ a <<= b
 a >>= b
 a += yield
 
+
+
 (a) += 1
 a[1] += 1
 a.b += 1
 a.b.c += 1
 f(i for i in range(2)).a += 1
 f().a += 1
+
+
 
 (a) = 1
 a.b = 1
@@ -33,6 +37,7 @@ a[:1] = b
 a[1:10:2] = b
 
 
+
 a: int = b
 a: int = yield
 a.b: int
@@ -41,19 +46,32 @@ a[b]: int = 1
 a[b]: int = 1
 a = 1
 a = 1.0
+
+
+
 a = ""
 a = r"\c"
 a = b"a"
 a = f"{a}"
 a = f"{d}" "rr"
+a = "rr" f"{d}" "rr"
+
+
+
 a = ()
 a = (1,)
 a = (1, 2)
+
+
+
 b = []
 b = [
     1,
 ]
 b = [1, 2]
+
+
+
 c = {
     1,
 }
@@ -64,13 +82,23 @@ d = {
     1: 2,
 }
 d = {1: 2, 3: 4}
+
+
+
 a = True
 b = False
 c = None
+
+
+
 d = *a, (*b, c)
 d = *a, (*b, *c)
 
+
+
 f = (a := 1)
+
+
 
 a, b = c
 a, *b = c
