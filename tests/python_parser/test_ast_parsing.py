1e767141b324d5fb3b82cef5785f48559e0369c7
MatthieuDartiailh <m.dartiailh@gmail.com>
2023-09-25 19:13:27 +0200

    python.gram: fix issues related to f-strings

--- tests/python_parser/test_ast_parsing.py
+++ tests/python_parser/test_ast_parsing.py
@@ -24,6 +24,7 @@ import pytest
         "call.py",
         "comprehensions.py",
         "expressions.py",
+        "fstrings.py",
         "function_def.py",
         "imports.py",
         "lambdas.py",
